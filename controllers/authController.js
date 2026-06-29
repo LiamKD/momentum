6225098d28550d3fb5efd1a58a68d7a3875a8ca7
LiamKD <liamkdoherty@gmail.com>
2026-06-29 21:05:33 +0100

    Implement user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,54 @@
 const path = require("path");
+const bcrypt = require("bcrypt");
+const db = require("../database/db");
 
 const showRegisterPage = (req, res) => {
     res.sendFile(path.join(__dirname, "../public/register.html"));
 };
 
+const registerUser = async (req, res) => {
+
+    const { firstName, lastName, email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+        return res.send("Passwords do not match.");
+    }
+
+    try {
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const sql = `
+            INSERT INTO users
+            (first_name, last_name, email, password_hash)
+            VALUES (?, ?, ?, ?)
+        `;
+
+        db.query(
+            sql,
+            [firstName, lastName, email, hashedPassword],
+            (err) => {
+
+                if (err) {
+                    console.log(err);
+                    return res.send("Registration failed.");
+                }
+
+                res.send("Registration successful!");
+
+            }
+        );
+
+    } catch (error) {
+
+        console.log(error);
+        res.send("Something went wrong.");
+
+    }
+
+};
+
 module.exports = {
-    showRegisterPage
+    showRegisterPage,
+    registerUser
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.get("/register", authController.showRegisterPage);
+router.post("/register", authController.registerUser);
 
 module.exports = router;
